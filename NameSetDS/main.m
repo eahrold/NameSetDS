2b3c7833d0467b39cd264ad9510e60537ad799ef
Eldon Ahrold <eldon.ahrold@gmail.com>
2013-08-01 09:25:19 -0500

    Initial Commit

---
+++ NameSetDS/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  NameSetDS
+//
+//  Created by Eldon Ahrold on 8/1/13.
+//  Copyright (c) 2013 Eldon Ahrold. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
